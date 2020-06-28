11807c08d887255a9a3a4217f7152b34223d8817
kmgk <hiragi.kame@gmail.com>
2020-06-28 15:30:48 +0900

    signInPageの実装

--- lib/view/eco_timeline/eco_timeline_page.dart
+++ lib/view/eco_timeline/eco_timeline_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hack20/common_widget/appbar.dart';
 import 'package:flutter_hack20/common_widget/post_card.dart';
 import 'package:flutter_hack20/model/ecoPost.dart';
-import 'package:flutter_hack20/model/user.dart';
 import 'package:provider/provider.dart';
 
 class EcoTimelinePage extends StatelessWidget {
@@ -10,7 +9,10 @@ class EcoTimelinePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final List<EcoPost> ecoPostList = Provider.of<List<EcoPost>>(context);
-    final User user = Provider.of<User>(context);
+
+    if (ecoPostList == null || ecoPostList.isEmpty) {
+      return const Scaffold(body: Center(child: CircularProgressIndicator()));
+    }
 
     return Scaffold(
       appBar: commonAppBar('eco posts', color: Colors.green),
